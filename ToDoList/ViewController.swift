a14658bc0e16f44c47b601a63654fff2dfa5e070
iambrbnkn <hurmavbatone@gmail.com>
2023-01-12 00:01:19 +0300

    1.0 ver.

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -5,15 +5,79 @@
 //  Created by Vitaliy on 11.01.2023.
 //
 
+//TODO:
+//- Add useralert for empty input
+//- Refactor code for reusability
+//- Add tasks mark with "star"
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let table: UITableView = {
+       let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
+    var items = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
+        title = "To Do List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.addSubview(table)
+        table.dataSource = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
     }
 
+    @objc func addButtonTapped() {
+        let alert = UIAlertController(title: "Add new note", message: "Enter in field below", preferredStyle: .alert)
+        alert.addTextField { (field) in
+            field.placeholder = "Write something"
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak self] (_) in
+            if let field = alert.textFields?.first {
+                if let text = field.text, !text.isEmpty {
+                    DispatchQueue.main.async {
+                        var currentItem = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                        currentItem.append(text)
+                        UserDefaults.standard.setValue(currentItem, forKey: "items")
+                        self?.items.append(text)
+                        self?.table.reloadData()
+                    }
+                }
+            }
+        }))
+        present(alert, animated: true)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
+    }
 
 }
 
+
+
+/// MARK: - ViewController extension
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = items[indexPath.row]
+        cell.contentConfiguration = config
+        return cell
+    }
+    
+    
+}
